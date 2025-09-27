f40799fb5d1d9bbfd9376fa59ecdfe107c2293e4
hardusss <danylo29bro@gmail.com>
2025-09-27 21:37:38 +0300

    Added point for like comment

--- backend/NextVibeAPI/posts/urls.py
+++ backend/NextVibeAPI/posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .view_pac import PostMenuView, LikePostView, PostViewSet
+from .view_pac import PostMenuView, LikePostView, PostViewSet, LikeCommentView
 from .view_pac import (
     CommentCreateView, CommentReplyView,
     GetCommentView, AddMediaToPostView,
@@ -16,6 +16,7 @@ urlpatterns = [
     path("add-media/", AddMediaToPostView.as_view(), name="add_media"),
     path('posts-menu/<int:id>/', PostMenuView.as_view(), name='posts_menu'),
     path("post-like/<int:id>/<int:post_id>/", LikePostView.as_view(), name="post_like"),
+    path("comment-like/<int:post_id>/<int:comment_id>/", LikeCommentView.as_view(), name="comment_like"),
     path("comment-create/", CommentCreateView.as_view(), name="comment_create"),
     path("comment-reply/<int:comment_id>/", CommentReplyView.as_view(), name="comment_reply"),
     path("get-comments/<int:post_id>/", GetCommentView.as_view(), name="get_comments"),
