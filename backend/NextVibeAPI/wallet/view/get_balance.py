d8af3abfa7c5732a80abe68516539e798bff12fc
hardusss <danylo29bro@gmail.com>
2025-10-09 15:29:05 +0300

    perf(wallet): improve balance fetching speed using async httpx
    Reduced balance fetching time from ~8s to ~1s by switching from requests to httpx with async support.

--- backend/NextVibeAPI/wallet/view/get_balance.py
+++ backend/NextVibeAPI/wallet/view/get_balance.py
@@ -9,74 +9,88 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
 from ..models import UserWallet
-
 from ..src.get_tokens_price import get_tokens_prices
 
+import asyncio
+from functools import partial
 
 User = get_user_model()
 
-def convert_scientific_to_decimal(scientific_number):
-    try:
-        if scientific_number == 0.0:
-            return "0.0"
-        decimal_number = float(scientific_number) 
-        return f"{decimal_number:.10f}".rstrip('0').rstrip('.')
-    except ValueError:
-        return scientific_number
-    
+def convert_scientific_to_decimal(x: float) -> str:
+    return f"{x:.10f}".rstrip('0').rstrip('.') if x else "0.0"
+
+async def get_all_balances(add_and_tokens: dict):
+    loop = asyncio.get_event_loop()
+    tasks = []
+
+    for key, Method in add_and_tokens.items():
+        if Method == BtcAddressBalance:
+            func = partial(Method.get_balance, wallet_name=key)
+        else:
+            func = partial(Method.get_balance, address=key)
+        tasks.append(loop.run_in_executor(None, func))
+
+    balances = await asyncio.gather(*tasks)
+    return balances
+
+
+
 class GetBalanceWallet(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request) -> Response:
-        user = User.objects.get(user_id=request.user.user_id) 
+        user = User.objects.get(user_id=request.user.user_id)
         wallet = UserWallet.objects.get(user=user)
 
         btc_wallet = wallet.btc_wallet
         sol_wallet = wallet.sol_wallet
         trx_wallet = wallet.trx_wallet
 
-        btc_balance = BtcAddressBalance.get_balance(btc_wallet.wallet_name)
-        sol_balance = SolAddressBalance.get_balance(address=sol_wallet.address)
-        trx_balance = TrxAddressBalance.get_balance(address=trx_wallet.address)
+        for_balances = {
+            btc_wallet.wallet_name: BtcAddressBalance,
+            sol_wallet.address: SolAddressBalance,
+            trx_wallet.address: TrxAddressBalance
+        }
 
-        prices = get_tokens_prices()
+        prices = asyncio.run(get_tokens_prices())
+        balances = asyncio.run(get_all_balances(for_balances))
+        btc_balance, sol_balance, trx_balance = balances
+
         btc_usdt = btc_balance * prices["bitcoin"]
         sol_usdt = sol_balance * prices["solana"]
         trx_usdt = trx_balance * prices["tron"]
-        
+
         total = round(btc_usdt + sol_usdt + trx_usdt, 2)
         data = [
-                total,
-                {
-                    "btc": {
-                        "address": btc_wallet.address,
-                        "icon": "https://cdn-icons-png.flaticon.com/512/5968/5968260.png",
-                        "amount": convert_scientific_to_decimal(btc_balance),
-                        "usdt": btc_usdt,
-                        "name": "Bitcoin",
-                        "symbol": "BTC",
-                        "price": prices["bitcoin"]
-                    },
-                    "sol": {
-                        "address": sol_wallet.address,
-                        "icon": "https://cdn-icons-png.flaticon.com/512/15208/15208206.png",
-                        "amount": convert_scientific_to_decimal(sol_balance),
-                        "usdt": sol_usdt,
-                        "name": "Solana",
-                        "symbol": "SOL",
-                        "price": prices["solana"]
-                    },
-                    "trx": {
-                        "address": trx_wallet.address,
-                        "icon": "https://cdn-icons-png.flaticon.com/512/15208/15208490.png",
-                        "amount": convert_scientific_to_decimal(trx_balance),
-                        "usdt": trx_usdt,
-                        "name": "Tron",
-                        "symbol": "TRX",
-                        "price": prices["tron"]
-                    },
-                }
-            ]
+            total,
+            {
+                "btc": {
+                    "address": btc_wallet.address,
+                    "icon": "https://cdn-icons-png.flaticon.com/512/5968/5968260.png",
+                    "amount": convert_scientific_to_decimal(btc_balance),
+                    "usdt": btc_usdt,
+                    "name": "Bitcoin",
+                    "symbol": "BTC",
+                    "price": prices["bitcoin"]
+                },
+                "sol": {
+                    "address": sol_wallet.address,
+                    "icon": "https://cdn-icons-png.flaticon.com/512/15208/15208206.png",
+                    "amount": convert_scientific_to_decimal(sol_balance),
+                    "usdt": sol_usdt,
+                    "name": "Solana",
+                    "symbol": "SOL",
+                    "price": prices["solana"]
+                },
+                "trx": {
+                    "address": trx_wallet.address,
+                    "icon": "https://cdn-icons-png.flaticon.com/512/15208/15208490.png",
+                    "amount": convert_scientific_to_decimal(trx_balance),
+                    "usdt": trx_usdt,
+                    "name": "Tron",
+                    "symbol": "TRX",
+                    "price": prices["tron"]
+                },
+            }
+        ]
         return Response(data, status=status.HTTP_200_OK)
-        
-        
