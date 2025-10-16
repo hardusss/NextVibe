ba6df5ac86806a8cebd69bfa7464808edbf690a7
hardusss <danylo29bro@gmail.com>
2025-10-16 19:29:20 +0300

    feat(posts): add method for delete post

---
+++ backend/NextVibeAPI/posts/view_pac/delete_post.py
@@ -0,0 +1,23 @@
+"""Method for delete post"""
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from ..models import Post
+
+class DeletePost(APIView):
+    permission_classes = [IsAuthenticated]
+    def delete(self, request) -> Response:
+        post_id: int = request.query_params.get("postId", 0)
+
+        # Validation
+        if post_id == 0:
+            return Response({"error": "Post not foud, check post id"}, status=status.HTTP_404_NOT_FOUND)
+        
+        post = Post.objects.get(id=post_id)
+        if post and post.owner__user_id == request.user.user_id:
+            try:
+                post.delete()
+                return Response({"data": "Post deleted"}, status=status.HTTP_200_OK)
+            except Exception as ex:
+                return Response({"error":f"Can't delete the post. Error: {ex}"})
