9a730e97b157f529531f0f1ed56219f514ae1c13
hardusss <danylo29bro@gmail.com>
2025-10-24 20:09:50 +0300

    perf: swap get messages from drf to fastapi

--- frontend/NextVibe/src/api/chat.ts
+++ frontend/NextVibe/src/api/chat.ts
@@ -108,10 +108,11 @@ export const getOnlineUsers = async () => {
 
 export const getMessages = async (chatId: number, lastMessageId?: number) => {
   const token = await AsyncStorage.getItem('access');
+  const user_id = await AsyncStorage.getItem("id")
   try {
     const url = lastMessageId 
-      ? `${GetApiUrl()}/chat/messages/${chatId}/?last_message_id=${lastMessageId}`
-      : `${GetApiUrl()}/chat/messages/${chatId}/`;
+      ? `${GetApiUrl().replace(":8000", ":8081")}/messages/${chatId}?last_message_id=${lastMessageId}&user_id=${user_id}`
+      : `${GetApiUrl().replace(":8000", ":8081")}/messages/${chatId}?user_id=${user_id}`;
       
     const response = await axios.get(url, {
       headers: {
