36002027847f98da6eb7c3feac13afcbd2378537
hardusss <danylo29bro@gmail.com>
2025-10-30 23:26:40 +0200

    feat: add method which check are all wallets exsists if no this wallet create

--- backend/NextVibeAPI/wallet/view/__init__.py
+++ backend/NextVibeAPI/wallet/view/__init__.py
@@ -1,3 +1,4 @@
 from .create_wallet import CreateWallet
 from .get_balance import GetBalanceWallet
 from .transaction import BtcTransactionView, SolTransactionView, TrxTrasactionView, AllTransactionsView, EthTrasactionView
+from .wallet_initializer import WalletInitializer
