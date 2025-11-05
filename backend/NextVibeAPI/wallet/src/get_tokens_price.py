72c23d1f61d469c78612fe3019e66126f5609723
hardusss <danylo29bro@gmail.com>
2025-11-05 02:00:25 +0200

    fix: fixed prices for last transaction

--- backend/NextVibeAPI/wallet/src/get_tokens_price.py
+++ backend/NextVibeAPI/wallet/src/get_tokens_price.py
@@ -1,39 +1,43 @@
 import httpx
-from typing import List, Dict
+from collections.abc import Sequence
 from django.core.cache import cache
 
 DEFAULT_TOKENS = ["solana", "tron", "ethereum"]
 
-async def get_tokens_prices(tokens: List[str] = DEFAULT_TOKENS, vs_currencies: str = "usd") -> Dict[str, float] | None:
-    
-    data = cache.get("prices")
-    data_last = cache.get("price_last")
-    if data and tokens == DEFAULT_TOKENS:
-        return data
-    if data_last:
-        return data_last
+async def get_tokens_prices(tokens: Sequence[str] | None = None, vs_currencies: str = "usd", last: bool = False) -> dict[str, float] | None:
+    if tokens is None:
+        tokens = DEFAULT_TOKENS
+
+    if last and len(tokens) != 1:
+        raise ValueError("Parameter 'last=True' is only supported for a single token.")
+
+    if not last and tokens == DEFAULT_TOKENS:
+        data = cache.get("prices")
+        if data:
+            return data
+
+    if last:
+        data_last = cache.get(f"price_last_{tokens[0]}")
+        if data_last:
+            return data_last
 
     url = "https://api.coingecko.com/api/v3/simple/price"
     params = {
         "ids": ",".join(tokens),
-        "vs_currencies": vs_currencies
+        "vs_currencies": vs_currencies,
     }
 
     async with httpx.AsyncClient(timeout=10) as client:
         response = await client.get(url, params=params)
         if response.status_code == 200:
             prices = response.json()
-            data: Dict[str, float] = {}
-            for token in tokens:
-                data[token] = prices[token][vs_currencies]
+            data = {token: prices[token][vs_currencies] for token in tokens}
 
-            if len(data) == 1:
-                cache.set("price_last", data, timeout=60)
+            if last and len(data) == 1:
+                cache.set(f"price_last_{tokens[0]}", data, timeout=60)
             else:
                 cache.set("prices", data, timeout=60)
 
             return data
-        if response.status_code != 200:
-            return cache.get("prices") or None
         else:
-            return None
+            return cache.get("prices")
