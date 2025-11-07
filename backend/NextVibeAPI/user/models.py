b12dee6141ac44e71f666b694625e7f74009bf58
hardusss <danylo29bro@gmail.com>
2025-11-07 16:51:45 +0200

    feat: created model for notification in app

--- backend/NextVibeAPI/user/models.py
+++ backend/NextVibeAPI/user/models.py
@@ -65,3 +65,32 @@ class User(AbstractBaseUser):
 class HistorySearch(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_history")
     searched_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="searched_user")
+
+class Notification(models.Model):
+    NOTIFICATION_TYPES = (
+        ('like', 'Like'),
+        ('comment', 'Comment'),
+        ('follow', 'Follow'),
+        ('revived_transaction', "Recived Transaction"),
+        ("deleted_post", "Post Deleted")
+    )
+
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sent_notifications"
+    )
+    recipient = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="received_notifications"
+    )
+    notification_type = models.CharField(max_length=20, choices=NOTIFICATION_TYPES)
+    post = models.ForeignKey(
+        "posts.Post", on_delete=models.CASCADE, null=True, blank=True, related_name="notifications"
+    )
+    text_preview = models.CharField(max_length=255, blank=True)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.sender} -> {self.recipient} ({self.notification_type})"
