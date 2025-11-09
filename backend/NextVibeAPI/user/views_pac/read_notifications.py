435fac0a8906f84ae3c479cf765d7f85285eb090
hardusss <danylo29bro@gmail.com>
2025-11-09 21:10:22 +0200

    fix: use a ready-made method for clear notifications cache

--- backend/NextVibeAPI/user/views_pac/read_notifications.py
+++ backend/NextVibeAPI/user/views_pac/read_notifications.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from user.models import Notification
-from django.core.cache import cache
+from user.src.clear_notify_cache import clear_notification_cache
 
 
 class ReadNotificationsView(APIView):
@@ -19,16 +19,7 @@ class ReadNotificationsView(APIView):
                 {"detail": "You have no unread notifications."},
                 status=status.HTTP_200_OK
             )
-
-        redis_client = getattr(cache, "client", None)
-        if redis_client:
-            client = redis_client.get_client()
-            pattern = f"user_{user.user_id}_notifications_page_*"
-            keys = client.keys(pattern)
-            if keys:
-                client.delete(*keys)
-
-        cache.delete(f"user_{user.user_id}_notifications_count")
+        clear_notification_cache(user.user_id)
 
         return Response(
             {"detail": f"{updated_count} notifications marked as read."},
