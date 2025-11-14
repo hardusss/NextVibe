89a5c868330bdcd3a0c6cd0413b05feb039e9bb2
hardusss <danylo29bro@gmail.com>
2025-11-14 23:54:06 +0200

    feat: method for check moderations post when start celery

--- backend/NextVibeAPI/NextVibeAPI/celery.py
+++ backend/NextVibeAPI/NextVibeAPI/celery.py
@@ -1,8 +1,14 @@
 import os
 from celery import Celery
+from celery.signals import worker_ready
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NextVibeAPI.settings")
 
 app = Celery("NextVibeAPI")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+@worker_ready.connect
+def at_start(sender, **kwargs):
+    from posts.tasks import auto_moderation_check
+    auto_moderation_check.delay()
