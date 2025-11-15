fbcc169ade871b43304c64892cd4f2a292a7c2b4
hardusss <danylo29bro@gmail.com>
2025-11-15 12:23:24 +0200

    feat: update logic return media url

--- backend/NextVibeAPI/NextVibeAPI/storages.py
+++ backend/NextVibeAPI/NextVibeAPI/storages.py
@@ -1,12 +1,13 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
 
 class R2Storage(S3Boto3Storage):
     default_acl = "public-read"
     file_overwrite = False
+    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
 
     def url(self, name):
         url = super().url(name)
         if not url.startswith("http"):
             return f"{self.custom_domain}/{name}"
-
         return url
