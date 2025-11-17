496b71e4451671133b5c1688b9bb340256ff1afa
hardusss <danylo29bro@gmail.com>
2025-11-18 00:29:46 +0200

    feat: add to return user liked posts

--- backend/NextVibeAPI/posts/view_pac/get_recomend.py
+++ backend/NextVibeAPI/posts/view_pac/get_recomend.py
@@ -1,9 +1,10 @@
-from .recomendations import Recomendations, RecomendationsFormater
+from .recomendations import RecomendationsFormater
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
+
 class RecomendationsView(APIView):
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticated]
     def get(self, request) -> Response:
         rec = RecomendationsFormater([128, 123, 131, 125, 126], 
                                     [162, 180, 172, 210, 192],
@@ -11,4 +12,4 @@ class RecomendationsView(APIView):
                                     [122, 132, 140, 142, 129, 134, 138])
         posts = rec.format()
         
-        return Response({"data": posts})
+        return Response({"data": posts, "liked_posts": request.user.liked_posts}, status=200)
