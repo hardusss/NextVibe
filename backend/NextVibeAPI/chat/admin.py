8031fb5a5ec928d4cf72ce4b10cee9a84b1964ce
hardusss <danylo29bro@gmail.com>
2025-11-18 23:23:50 +0200

    feat: add showing all data even user is baned

--- backend/NextVibeAPI/chat/admin.py
+++ backend/NextVibeAPI/chat/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Chat, Message, MediaAttachment
+from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-admin.site.register(Chat)
-admin.site.register(Message)
-admin.site.register(MediaAttachment)
+User = get_user_model()
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return Chat.all_objects.all()
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return Message.all_objects.all()
+
+@admin.register(MediaAttachment)
+class MediaAttachmentAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return MediaAttachment.all_objects.all()
--- backend/NextVibeAPI/user/admin.py
+++ backend/NextVibeAPI/user/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import User, HistorySearch, Notification
+from django.contrib import admin
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "is_online", "is_baned"]
+
+    list_filter = ('is_baned', 'is_staff', 'is_superuser', 'is_active')
+
+    search_fields = ('username', 'email')
+
+    def get_queryset(self, request):
+        return User.all_objects.all()
+
 admin.site.register(HistorySearch)
 admin.site.register(Notification)
