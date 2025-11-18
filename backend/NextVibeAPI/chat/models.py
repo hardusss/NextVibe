8c646e1f1eefd1e0ba07f79a7b0a44d84aef12d4
hardusss <danylo29bro@gmail.com>
2025-11-18 23:22:33 +0200

    feat: add managers to models for not showing data if user is baned

--- backend/NextVibeAPI/chat/models.py
+++ backend/NextVibeAPI/chat/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from .managers import ChatManager, MessageManager, MediaAttachmentManager
 
 User = get_user_model()
 
@@ -7,6 +8,9 @@ class Chat(models.Model):
     participants = models.ManyToManyField(User, related_name='chats')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ChatManager()
+    all_objects = models.Manager()
+
     def __str__(self):
         return f"Chat {self.id}"
 
@@ -17,6 +21,8 @@ class Message(models.Model):
     text = models.TextField(blank=True, null=True, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+    objects = MessageManager()
+    all_objects = models.Manager()
 
     def __str__(self):
         return f"Message {self.id} in Chat {self.chat.id}"
@@ -24,6 +30,8 @@ class Message(models.Model):
 class MediaAttachment(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='media')
     file = models.FileField(upload_to='chat_media/')
+    objects = MediaAttachmentManager()
+    all_objects = models.Manager()
 
     def __str__(self):
         return f"Media for Message {self.message.id}"
