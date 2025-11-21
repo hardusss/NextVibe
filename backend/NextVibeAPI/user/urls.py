b1a1cd3c5067ef5cdaf739d665f49ee043b2fae2
hardusss <danylo29bro@gmail.com>
2025-11-21 21:45:15 +0200

    feat: add urls for get tokens

--- backend/NextVibeAPI/user/urls.py
+++ backend/NextVibeAPI/user/urls.py
@@ -11,9 +11,14 @@ from .views_pac import (
                             GetNotificationsView, ReadNotificationsView,
                             CheckStatusView
                         )
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("register/", RegisterUserView.as_view(), name='register_user'),
     path("login/", LoginUserView.as_view(), name='login_user'),
     path("google-register/", GoogleRegisterView.as_view(), name="google_register"),
