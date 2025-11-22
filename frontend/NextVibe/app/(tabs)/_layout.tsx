48d9a406f9c01b78b57a5fa14c53f68f554db411
hardusss <danylo29bro@gmail.com>
2025-11-22 22:41:18 +0200

    feat: connect error boundary for show custom error page

--- frontend/NextVibe/app/(tabs)/_layout.tsx
+++ frontend/NextVibe/app/(tabs)/_layout.tsx
@@ -9,6 +9,8 @@ import { storage } from "@/src/utils/storage";
 import { WebSocketProvider } from "@/src/context/WebSocketContext";
 import axios from "axios";
 import Web3Toast from "@/components/Shared/Toasts/Web3Toast";
+import ErrorBoundary from 'react-native-error-boundary';
+import ErrorFallback from "@/components/ErrorFallback";
 
 if (__DEV__ && typeof global !== 'undefined') {
   const originalError = console.error;
@@ -119,99 +121,101 @@ export default function Layout() {
   const showTabBar = ![...blacklist, "camera"].includes(currentPage);
 
   return (
-    <WebSocketProvider userId={userID || 0}>
-      {toastMessage && (
-        <Web3Toast
-          message={toastMessage}
-          visible={visible}
-          onHide={() => setVisible(false)}
-          isSuccess={false}
-        />
-      )}
-      <View style={{ flex: 1 }}>
-        <Stack screenOptions={{ headerShown: false, animation: "none" }} >
-          <Stack.Screen name="home" />
-          <Stack.Screen name="search" />
-          <Stack.Screen name="camera" />
-          <Stack.Screen name="profile" />
-          {blacklist.map((item) => <Stack.Screen key={item} name={item} />)}
-        </Stack>
-
-        {showTabBar && (
-          <View style={{
-            position: "absolute",
-            bottom: 10,
-            width: "90%",
-            marginLeft: "5%",
-            height: 60,
-            flexDirection: "row",
-            justifyContent: "space-around",
-            alignItems: "center",
-            borderRadius: 20,
-            borderColor: theme === "dark" ? "rgba(26,26,47,0.95)" : "#D9D9D9",
-            backgroundColor: theme === "dark" ? "#1A1A2F" : "#D9D9D9",
-            shadowColor: "transparent",
-          }}>
-            {tabs.map((tab) => (
-              <TouchableOpacity key={tab.name} onPress={() => goToTab(tab.name)}>
-                {tab.name === "profile" && imageProfile ? (
-                  <FastImage
-                    source={{ uri: imageProfile, priority: FastImage.priority.normal, cache: FastImage.cacheControl.immutable }}
-                    style={{
-                      width: 25,
-                      height: 25,
-                      borderRadius: 50,
-                      borderWidth: currentPage === "profile" ? 2 : 0,
-                      borderColor: "#7305f0ff",
-                    }}
-                  />
-                ) : tab.name === "search" ? (
-                  <View style={{
-                    position: "relative",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: currentPage === "search" ? (theme === "dark" ? "#D9D9D9" : "#1A1A2F") : (theme === "dark" ? "#1A1A2F" : "#D9D9D9"),
-                    width: 40,
-                    height: 40,
-                    borderRadius: 50,
-                  }}>
-                    <View style={{
-                      width: 8,
-                      height: 8,
-                      borderRadius: 20,
-                      backgroundColor: currentPage === "search" ? (theme === "dark" ? "#1A1A2F" : "#D9D9D9") : "transparent",
-                      position: "absolute",
-                      left: -6,
-                      bottom: -2
-                    }} />
-                    <MaterialIcons
-                      name="search"
-                      size={20}
-                      color={currentPage === "search" ? (theme === "dark" ? "#1A1A2F" : "#D9D9D9") : (theme === "dark" ? "#D9D9D9" : "#1A1A2F")}
-                      style={{ zIndex: 9999 }}
-                    />
-                  </View>
-                ) : (
-                  <View style={{
-                    backgroundColor: currentPage === tab.name ? (theme === "dark" ? "#D9D9D9" : "#1A1A2F") : (theme === "dark" ? "#1A1A2F" : "#D9D9D9"),
-                    width: 40,
-                    height: 40,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: 50,
-                  }}>
-                    <tab.icon
-                      name={currentPage === tab.name ? tab.iconName[1] : tab.iconName[0]}
-                      size={20}
-                      color={currentPage === tab.name ? (theme === "dark" ? "#1A1A2F" : "#D9D9D9") : inactiveColor}
-                    />
-                  </View>
-                )}
-              </TouchableOpacity>
-            ))}
-          </View>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <WebSocketProvider userId={userID || 0}>
+        {toastMessage && (
+          <Web3Toast
+            message={toastMessage}
+            visible={visible}
+            onHide={() => setVisible(false)}
+            isSuccess={false}
+          />
         )}
-      </View>
-    </WebSocketProvider>
+        <View style={{ flex: 1 }}>
+          <Stack screenOptions={{ headerShown: false, animation: "none" }} >
+            <Stack.Screen name="home" />
+            <Stack.Screen name="search" />
+            <Stack.Screen name="camera" />
+            <Stack.Screen name="profile" />
+            {blacklist.map((item) => <Stack.Screen key={item} name={item} />)}
+          </Stack>
+
+          {showTabBar && (
+            <View style={{
+              position: "absolute",
+              bottom: 10,
+              width: "90%",
+              marginLeft: "5%",
+              height: 60,
+              flexDirection: "row",
+              justifyContent: "space-around",
+              alignItems: "center",
+              borderRadius: 20,
+              borderColor: theme === "dark" ? "rgba(26,26,47,0.95)" : "#D9D9D9",
+              backgroundColor: theme === "dark" ? "#1A1A2F" : "#D9D9D9",
+              shadowColor: "transparent",
+            }}>
+              {tabs.map((tab) => (
+                <TouchableOpacity key={tab.name} onPress={() => goToTab(tab.name)}>
+                  {tab.name === "profile" && imageProfile ? (
+                    <FastImage
+                      source={{ uri: imageProfile, priority: FastImage.priority.normal, cache: FastImage.cacheControl.immutable }}
+                      style={{
+                        width: 25,
+                        height: 25,
+                        borderRadius: 50,
+                        borderWidth: currentPage === "profile" ? 2 : 0,
+                        borderColor: "#7305f0ff",
+                      }}
+                    />
+                  ) : tab.name === "search" ? (
+                    <View style={{
+                      position: "relative",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      backgroundColor: currentPage === "search" ? (theme === "dark" ? "#D9D9D9" : "#1A1A2F") : (theme === "dark" ? "#1A1A2F" : "#D9D9D9"),
+                      width: 40,
+                      height: 40,
+                      borderRadius: 50,
+                    }}>
+                      <View style={{
+                        width: 8,
+                        height: 8,
+                        borderRadius: 20,
+                        backgroundColor: currentPage === "search" ? (theme === "dark" ? "#1A1A2F" : "#D9D9D9") : "transparent",
+                        position: "absolute",
+                        left: -6,
+                        bottom: -2
+                      }} />
+                      <MaterialIcons
+                        name="search"
+                        size={20}
+                        color={currentPage === "search" ? (theme === "dark" ? "#1A1A2F" : "#D9D9D9") : (theme === "dark" ? "#D9D9D9" : "#1A1A2F")}
+                        style={{ zIndex: 9999 }}
+                      />
+                    </View>
+                  ) : (
+                    <View style={{
+                      backgroundColor: currentPage === tab.name ? (theme === "dark" ? "#D9D9D9" : "#1A1A2F") : (theme === "dark" ? "#1A1A2F" : "#D9D9D9"),
+                      width: 40,
+                      height: 40,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      borderRadius: 50,
+                    }}>
+                      <tab.icon
+                        name={currentPage === tab.name ? tab.iconName[1] : tab.iconName[0]}
+                        size={20}
+                        color={currentPage === tab.name ? (theme === "dark" ? "#1A1A2F" : "#D9D9D9") : inactiveColor}
+                      />
+                    </View>
+                  )}
+                </TouchableOpacity>
+              ))}
+            </View>
+          )}
+        </View>
+      </WebSocketProvider>
+    </ErrorBoundary>
   );
 }
