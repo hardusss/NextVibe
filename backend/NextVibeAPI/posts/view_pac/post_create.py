70f6bb1a5be94b7cbb0cad0029255e2e41c31cb2
hardusss <danylo29bro@gmail.com>
2025-11-22 18:59:54 +0200

    feat: add method finalize_creation for when all media uploaded and compresed send all for moderation

--- backend/NextVibeAPI/posts/view_pac/post_create.py
+++ backend/NextVibeAPI/posts/view_pac/post_create.py
@@ -2,8 +2,11 @@ from rest_framework import viewsets
 from ..models import Post
 from ..serializers_pac import PostSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from rest_framework.throttling import ScopedRateThrottle
+from ..tasks import send_post_for_moderation
 
 User = get_user_model()
 
@@ -18,3 +21,31 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer.save(owner=self.request.user)
         self.request.user.post_count += 1
         self.request.user.save(update_fields=['post_count'])
+
+    @action(detail=True, methods=['post'], url_path='finalize')
+    def finalize_creation(self, request, pk=None):
+        """
+        Client call. when post and all medias uploaded.
+        Once start moderation.
+        """
+        post = self.get_object()
+        
+        # Check rules
+        if post.owner != request.user:
+            return Response(
+                {"error": "Not your post"}, 
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        # Update status
+        post.moderation_status = "pending"
+        post.save(update_fields=['moderation_status'])
+        
+        # Start Celery task
+        print(f"🚀 Finalize called for post {post.id}. Triggering moderation task.")
+        send_post_for_moderation.delay(post.id)
+        
+        return Response({
+            "status": "moderation_started",
+            "message": "Post submitted for review"
+        })
