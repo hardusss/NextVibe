8ac1a88bcd57abd7fe739c20dfeacc6e889a673c
hardusss <danylo29bro@gmail.com>
2025-11-27 23:52:32 +0200

    feat: migrate to new recommendation and set seen posts

--- frontend/NextVibe/src/api/get.recomendate.posts.ts
+++ frontend/NextVibe/src/api/get.recomendate.posts.ts
@@ -2,20 +2,23 @@ import axios from "axios";
 import { storage } from "../utils/storage";
 import GetApiUrl from "../utils/url_api";
 
-export default async function getRecomendatePosts () {
-
-    try {
-        const token = await storage.getItem('access');
-        const response = await axios.get(`${GetApiUrl()}/posts/recomendations/`, {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }  
-        })
-        return response.data;
-    }
-
-    catch (error) {
-        return null;
-    }
+export default async function getRecomendatePosts(
+  seenIds: number[] = []
+) {
+  try {
+    const token = await storage.getItem("access");
+    const response = await axios.get(`${GetApiUrl()}/posts/recommendation-feed/`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        // Only sending seen IDs is sufficient for the new backend logic
+        seen: seenIds.join(','),
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching recommended posts:", error);
+    return null;
+  }
 }
-
