d97b4f3910e3e3fe021b345cd2a7da3b602dba33
hardusss <danylo29bro@gmail.com>
2025-11-28 22:48:36 +0200

    feat: remove seen ids

--- frontend/NextVibe/src/api/get.recomendate.posts.ts
+++ frontend/NextVibe/src/api/get.recomendate.posts.ts
@@ -2,18 +2,12 @@ import axios from "axios";
 import { storage } from "../utils/storage";
 import GetApiUrl from "../utils/url_api";
 
-export default async function getRecomendatePosts(
-  seenIds: number[] = []
-) {
+export default async function getRecomendatePosts() {
   try {
     const token = await storage.getItem("access");
     const response = await axios.get(`${GetApiUrl()}/posts/recommendation-feed/`, {
       headers: {
         Authorization: `Bearer ${token}`,
-      },
-      params: {
-        // Only sending seen IDs is sufficient for the new backend logic
-        seen: seenIds.join(','),
       },
     });
     return response.data;
