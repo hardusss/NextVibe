2f91fcff6e75779d54d7963fbcd60d944512c0f1
hardusss <danylo29bro@gmail.com>
2025-11-30 18:51:30 +0200

    feat: sync posts_count with approved posts dynamically

--- backend/NextVibeAPI/user/serializers_pac/user_details.py
+++ backend/NextVibeAPI/user/serializers_pac/user_details.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from posts.models import Post
 
 User = get_user_model()
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
+    posts_count = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         exclude = ('password',)
+
+    def get_posts_count(self, obj):
+        # get the current number of posts
+        actual_count = Post.objects.filter(owner__user_id=obj.user_id, moderation_status="approved").count()
+        if obj.post_count != actual_count:
+            obj.post_count = actual_count
+            obj.save(update_fields=['post_count'])
+        
+        return actual_count
