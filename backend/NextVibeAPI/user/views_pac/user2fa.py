49d5d3ff65338c38b2926d0180bd39e8e3fa04f3
hardusss <danylo29bro@gmail.com>
2025-12-07 20:04:46 +0200

    feat: fix saving qr

--- backend/NextVibeAPI/user/views_pac/user2fa.py
+++ backend/NextVibeAPI/user/views_pac/user2fa.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import get_user_model
+from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
 from ..src import TwoFA
 from rest_framework.throttling import ScopedRateThrottle
@@ -9,33 +10,34 @@ from rest_framework.throttling import ScopedRateThrottle
 User = get_user_model()
 
 
-
 class TwoFAView(APIView):
     permission_classes = [IsAuthenticated]
     throttle_classes = [ScopedRateThrottle]
     throttle_scope = "2fa"
+
     def get(self, request, *args, **kwargs) -> Response:
         try:
             user = User.objects.get(user_id=request.user.user_id)
+            
             if user.secret_2fa:
-                return Response({"data": {"code": user.secret_2fa, "qrcode": f"/media/qrcodes/{user.email}_qrcode.png"}})
+                qr_url = f"https://{settings.AWS_S3_CUSTOM_DOMAIN}/qrcodes/{user.email}_qr_code.png"
+                return Response({"data": {"code": user.secret_2fa, "qrcode": qr_url}})
             else:
                 twoFa = TwoFA()
                 secret = twoFa.create_2fa(user.email)
                 user.secret_2fa = secret[0]
                 user.save()
                 return Response({
-                                "data": {
-                                    "code": secret[0],
-                                    "qrcode": secret[1]
-                                }
-                            }, status=status.HTTP_200_OK)
+                    "data": {
+                        "code": secret[0],
+                        "qrcode": secret[1] 
+                    }
+                }, status=status.HTTP_200_OK)
         except User.DoesNotExist:
             return Response({"data": "User doesn't exist"}, status=status.HTTP_404_NOT_FOUND)
-        
+
     def post(self, request, *args, **kwargs) -> Response:
         verify_code: int = int(request.query_params.get("verifyCode"))
-        
         try:
             user = User.objects.get(user_id=request.user.user_id)
             twoFa = TwoFA(secret_key=user.secret_2fa)
@@ -45,7 +47,7 @@ class TwoFAView(APIView):
                 return Response({"data": "Code not correct, try again"})
         except User.DoesNotExist:
             return Response({"data": "User doesn't exist"}, status=status.HTTP_404_NOT_FOUND)
-    
+
     def put(self, request, *args, **kwargs) -> Response:
         user_id = request.user.user_id
         is2FA = True if request.query_params.get("enable").lower() == "true" else False 
@@ -54,6 +56,5 @@ class TwoFAView(APIView):
             user.is2FA = is2FA
             user.save()
             return Response({"data": "Update success"}, status=status.HTTP_200_OK)
-        
         except User.DoesNotExist:
             return Response({"data": "User not found"}, status=status.HTTP_400_BAD_REQUEST)
