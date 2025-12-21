89e42c53817426464e422f23ee89abf4219641c5
hardusss <danylo29bro@gmail.com>
2025-12-21 23:16:52 +0200

    fix: eas update

--- frontend/NextVibe/app/(tabs)/index.tsx
+++ frontend/NextVibe/app/(tabs)/index.tsx
@@ -1,10 +1,29 @@
 import { Redirect } from "expo-router";
 import { LogBox } from 'react-native';
 import { setupAxiosInterceptor } from "@/src/utils/axiosInterceptor";
+import React, { useEffect } from 'react';
+import * as Updates from 'expo-updates';
 
 setupAxiosInterceptor();
 LogBox.ignoreAllLogs(true); 
 
 export default function Index() {
+     useEffect(() => {
+    (async () => {
+      try {
+        const update = await Updates.checkForUpdateAsync();
+        console.log('Update available?', update.isAvailable);
+
+        if (update.isAvailable) {
+          console.log('Fetching update...');
+          await Updates.fetchUpdateAsync();
+          console.log('Reloading app...');
+          await Updates.reloadAsync(); 
+        }
+      } catch (e) {
+        console.error('OTA error:', e);
+      }
+    })();
+  }, []);
     return <Redirect href="/splash" />;
 }
