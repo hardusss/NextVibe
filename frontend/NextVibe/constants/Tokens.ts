8d57ffd82c995aca2cd254a85e65869d53f310d9
hardusss <danylo29bro@gmail.com>
2026-01-09 17:14:59 +0200

    feat: add TOKENS nad set correct USDC_MINT

--- frontend/NextVibe/constants/Tokens.ts
+++ frontend/NextVibe/constants/Tokens.ts
@@ -1,3 +1,19 @@
-export const TOKEN_CONSTANTS = {
-  USDC_MINT: '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU', 
+export const TOKEN_MINT_CONSTANTS = {
+    // Circle
+    USDC_MINT: '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU', 
 };
+
+export const TOKENS = {
+    SOL: {
+      symbol: "SOL",
+      name: "Solana",
+      logoURL: "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
+      priceKey: 'solana' as const,
+    },
+    USDC: {
+      symbol: "USDC",
+      name: "USD Coin",
+      logoURL: "https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=040",
+      priceKey: 'usd-coin' as const,
+    },
+}
