96b354aa2f9b7d950e9e287181175e4e625ceb1f
hardusss <danylo29bro@gmail.com>
2026-02-21 15:55:44 +0200

    feat: add validate google token id

--- backend/NextVibeAPI/user/views_pac/google_registration.py
+++ backend/NextVibeAPI/user/views_pac/google_registration.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..serializers_pac import GoogleRegister
 from rest_framework.throttling import ScopedRateThrottle
+from user.src.validate_google_token_id import validate
+from django.contrib.auth import get_user_model
 
 
 class GoogleRegisterView(APIView):
@@ -12,10 +14,26 @@ class GoogleRegisterView(APIView):
     throttle_scope = "auth"
 
     def post(self, request, *args, **kwargs):
-        serializer = GoogleRegister(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+        idToken = request.data.get("idToken")
+        if not idToken:
+            return Response({"error": "Not found id token"}, status=400)
 
+        google_data = validate(idToken)
+        if not google_data or not google_data.get("email_verified"):
+            return Response({"error": "Token not valid or email not verified"}, status=400)
+
+        User = get_user_model()
+        user = User.objects.filter(email=google_data["email"]).first()
+
+        if not user:
+            serializer = GoogleRegister(data={
+                "email": google_data["email"],
+                "username": google_data.get("name"),
+                "avatar_url": google_data.get("avatar_url")
+            })
+            serializer.is_valid(raise_exception=True)
+            user = serializer.save()
+        else:
+            serializer = GoogleRegister(user)
+
+        return Response(serializer.data, status=200)
