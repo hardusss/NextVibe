f3989604edeb4f061889e59ce85ae0007bd90e43
hardusss <danylo29bro@gmail.com>
2026-02-22 22:30:08 +0200

    fix: wma

--- frontend/NextVibe/withSolanaMWA.js
+++ frontend/NextVibe/withSolanaMWA.js
@@ -2,20 +2,19 @@ const { withAndroidManifest } = require('@expo/config-plugins');
 
 const withSolanaMWA = (config) => {
   return withAndroidManifest(config, (config) => {
-    const androidManifest = config.modResults.manifest;
-
-    if (!androidManifest.queries) {
-      androidManifest.queries = [];
+    const manifest = config.modResults.manifest;
+    if (!manifest.queries) {
+      manifest.queries = [{}];
+    }
+    
+    if (!manifest.queries[0].intent) {
+      manifest.queries[0].intent = [];
     }
 
-    androidManifest.queries.push({
-      intent: [
-        {
-          action: [{ $: { 'android:name': 'android.intent.action.VIEW' } }],
-          category: [{ $: { 'android:name': 'android.intent.category.BROWSABLE' } }],
-          data: [{ $: { 'android:scheme': 'solana-wallet' } }],
-        },
-      ],
+    manifest.queries[0].intent.push({
+      action: [{ $: { 'android:name': 'android.intent.action.VIEW' } }],
+      category: [{ $: { 'android:name': 'android.intent.category.BROWSABLE' } }],
+      data: [{ $: { 'android:scheme': 'solana-wallet' } }],
     });
 
     return config;
