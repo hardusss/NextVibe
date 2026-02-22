b5aa3cb02b98db9b5227c3c081c5c493cf29e426
hardusss <danylo29bro@gmail.com>
2026-02-22 22:24:40 +0200

    fix wma

--- frontend/NextVibe/app/(tabs)/index.tsx
+++ frontend/NextVibe/app/(tabs)/index.tsx
@@ -3,35 +3,28 @@ import { LogBox } from 'react-native';
 import { setupAxiosInterceptor } from "@/src/utils/axiosInterceptor";
 import React, { useEffect } from 'react';
 import * as Updates from 'expo-updates';
-// Connect Buffer for LazorKit
-import 'react-native-get-random-values';
-import 'react-native-url-polyfill/auto';
-import { Buffer } from 'buffer';
-
-
-
-global.Buffer = global.Buffer || Buffer;
 
 setupAxiosInterceptor();
 LogBox.ignoreAllLogs(true); 
 
 export default function Index() {
-     useEffect(() => {
-    (async () => {
-      try {
-        const update = await Updates.checkForUpdateAsync();
-        console.log('Update available?', update.isAvailable);
+    useEffect(() => {
+        (async () => {
+            try {
+                const update = await Updates.checkForUpdateAsync();
+                console.log('Update available?', update.isAvailable);
 
-        if (update.isAvailable) {
-          console.log('Fetching update...');
-          await Updates.fetchUpdateAsync();
-          console.log('Reloading app...');
-          await Updates.reloadAsync(); 
-        }
-      } catch (e) {
-        console.error('OTA error:', e);
-      }
-    })();
-  }, []);
+                if (update.isAvailable) {
+                    console.log('Fetching update...');
+                    await Updates.fetchUpdateAsync();
+                    console.log('Reloading app...');
+                    await Updates.reloadAsync(); 
+                }
+            } catch (e) {
+                console.error('OTA error:', e);
+            }
+        })();
+    }, []);
+
     return <Redirect href="/splash" />;
 }
