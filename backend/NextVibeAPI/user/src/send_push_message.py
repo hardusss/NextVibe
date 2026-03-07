f47263638c4067e218159f31e56652ccc002541d
hardusss <danylo29bro@gmail.com>
2026-03-07 13:48:56 +0200

    feat: fixed push, return owner wallet etc

--- backend/NextVibeAPI/user/src/send_push_message.py
+++ backend/NextVibeAPI/user/src/send_push_message.py
@@ -1,26 +1,7 @@
 from exponent_server_sdk import (
-    DeviceNotRegisteredError,
     PushClient,
     PushMessage,
-    PushServerError,
-    PushTicketError,
 )
-from dotenv import load_dotenv
-import os, requests
-
-load_dotenv()
-
-session = requests.Session()
-
-session.headers.update(
-    {
-        "Authorization": f"Bearer {os.getenv('EXPO_TOKEN')}",
-        "accept": "application/json",
-        "accept-encoding": "gzip, deflate",
-        "content-type": "application/json",
-    }
-)
-
 
 def send(token: str, title: str, body: str):
     response = PushClient().publish(
