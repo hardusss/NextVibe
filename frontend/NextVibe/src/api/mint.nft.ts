78068c7b7792a8fef88b5fcb47414f342a34236b
hardusss <danylo29bro@gmail.com>
2026-03-07 13:56:39 +0200

    feat: add signature

--- frontend/NextVibe/src/api/mint.nft.ts
+++ frontend/NextVibe/src/api/mint.nft.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { storage } from "../utils/storage";
 import GetApiUrl from "../utils/url_api";
 
-export default async function mintNFT(walletAddress: string, post_id: number, price: number) {
+export default async function mintNFT(walletAddress: string, post_id: number, price: number, paymentSignature?: string) {
     const TOKEN = await storage.getItem("access");
 
     const url = `${GetApiUrl()}/posts/cnft-mint/`;
@@ -16,7 +16,8 @@ export default async function mintNFT(walletAddress: string, post_id: number, pr
     const response = await axios.post(url, {
         walletAddress,
         postId: post_id,
-        price
+        price,
+        paymentSignature
     }, config)
     return response.data
 }
