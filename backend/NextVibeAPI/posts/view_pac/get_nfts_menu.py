b8a9b6a24d1657b1c7ef90e7c6c88e43d518af50
hardusss <danylo29bro@gmail.com>
2026-03-08 16:14:24 +0200

    feat: add hide fields

--- backend/NextVibeAPI/posts/view_pac/get_nfts_menu.py
+++ backend/NextVibeAPI/posts/view_pac/get_nfts_menu.py
@@ -21,7 +21,7 @@ class UserCollectionView(APIView):
 
         collections_qs = (
             UserCollection.objects
-            .filter(user__user_id=user_id, is_hide=False, is_ai_generated=False)
+            .filter(user__user_id=user_id, post__is_hide=False, post__is_ai_generated=False)
             .select_related("post__owner")
             .prefetch_related(Prefetch("post__media", queryset=PostsMedia.objects.all()))
             .order_by("-minted_at")[index:index + limit]
