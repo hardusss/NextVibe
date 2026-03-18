c83fcd163bab6546f6bcbb5db9f6e26cd8ec27b5
hardusss <danylo29bro@gmail.com>
2026-03-18 10:26:19 +0200

    feat: new settings push

--- backend/NextVibeAPI/user/src/send_push_message.py
+++ backend/NextVibeAPI/user/src/send_push_message.py
@@ -2,7 +2,6 @@ from exponent_server_sdk import (
     PushClient,
     PushMessage,
 )
-
 from typing import Optional
 
 def send(token: str, title: str, body: str, link: Optional[str] = None):
@@ -13,15 +12,16 @@ def send(token: str, title: str, body: str, link: Optional[str] = None):
             title=title,
             body=body,
             data=data,
-            sound=None,
+            sound="default",         
             ttl=None,
             expiration=None,
-            priority=None,
+            priority="high",       
             badge=None,
             category=None,
-            display_in_foreground=None,
-            channel_id=None,
+            display_in_foreground=True, 
+            channel_id=None,         
             subtitle=None,
             mutable_content=None
         )
     )
+    print(f"Sent: {body}")
