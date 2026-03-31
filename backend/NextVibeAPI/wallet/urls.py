bfe21d862efff04ae20bd27ad4b2901876b4d453
hardusss <danylo29bro@gmail.com>
2026-03-31 20:36:05 +0300

    fix: delete legacy code

--- backend/NextVibeAPI/wallet/urls.py
+++ backend/NextVibeAPI/wallet/urls.py
@@ -1,24 +1,11 @@
 from django.urls import path
 from .view import (
-    CreateWallet, GetBalanceWallet,
-    BtcTransactionView, SolTransactionView,
-    TrxTrasactionView, EthTrasactionView,
-    AllTransactionsView, WalletInitializer,
-    GetTransactionFee, GetLastTransactionView,
+    GetTransactionFee,
     GetTokensPriceView
     )
 
 
 urlpatterns = [
-    path("wallet-init/", WalletInitializer.as_view(), name="wallet_init"),
-    path("create/", CreateWallet.as_view(), name="create_wallet"),
-    path("get-balance/", GetBalanceWallet.as_view(), name="balance_wallet"),
-    path("transactions/", AllTransactionsView.as_view(), name="all_transactions"),
-    path("transaction/last/", GetLastTransactionView.as_view(), name="last_transaction"),
-    path("transaction/btc/", BtcTransactionView.as_view(), name="transaction_btc"),
-    path("transaction/sol/", SolTransactionView.as_view(), name="transaction_sol"),
-    path("transaction/trx/", TrxTrasactionView.as_view(), name="transaction_trx"),
-    path("transaction/eth/", EthTrasactionView.as_view(), name="transaction_eth"),
     path("fee/", GetTransactionFee.as_view(), name="fee"),
     path("get-tokens-price/", GetTokensPriceView.as_view(), name="get_tokens_price")
 ]
--- backend/NextVibeAPI/wallet/view/__init__.py
+++ backend/NextVibeAPI/wallet/view/__init__.py
@@ -1,7 +1,2 @@
-from .create_wallet import CreateWallet
-from .get_balance import GetBalanceWallet
-from .transaction import BtcTransactionView, SolTransactionView, TrxTrasactionView, AllTransactionsView, EthTrasactionView
-from .wallet_initializer import WalletInitializer
 from .get_transaction_fee import GetTransactionFee
-from .get_last_transaction import GetLastTransactionView
 from .get_tokens_price_view import GetTokensPriceView
