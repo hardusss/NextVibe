d143e23478ea4e1b2301e9d0a27c06689698549e
hardusss <danylo29bro@gmail.com>
2026-04-08 12:40:21 +0300

    feat: method for get invite info

---
+++ frontend/NextVibe/src/api/get.invite.info.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { storage } from "../utils/storage";
+import GetApiUrl from "../utils/url_api";
+
+
+export default async function getInviteInfo() {
+    const TOKEN = await storage.getItem("access");
+
+    const url = `${GetApiUrl()}/users/invite-info/`;
+
+    const config = {
+        headers: {
+            "Authorization": `Bearer ${TOKEN}`
+        },
+
+    }
+
+    const response = await axios.get(url, config)
+    return response.data
+}
+
