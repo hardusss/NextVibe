86bab64975b9af46feb8d4446fcf3d94756b37d9
hardusss <danylo29bro@gmail.com>
2026-04-12 11:53:53 +0300

    feat: add check is og, if it's true return metadata for og collections

--- backend/NextVibeAPI/posts/view_pac/collection_metadata.py
+++ backend/NextVibeAPI/posts/view_pac/collection_metadata.py
@@ -5,6 +5,24 @@ from rest_framework import status
 
 class CollectionMetadataView(APIView):
     def get(self, request) -> Response:
+        is_og = request.query_params.get("isOg", "").lower() == "true"
+        if is_og:
+            metadata = {
+                "name": "NextVibe OG Status",
+                "symbol": "NVOG",
+                "description": "Official NextVibe og collection limited 25 cNFTs",
+                "image": "https://nextvibe.io/logo.png",
+                "properties": {
+                    "files": [
+                        {
+                            "uri": "https://nextvibe.io/logo.png",  
+                            "type": "image/png"                     
+                        }
+                    ],
+                    "category": "image",
+                },
+            }
+            return Response(metadata, status=status.HTTP_200_OK)
         metadata = {
             "name": "NextVibe Collection",
             "symbol": "NVIBE",
