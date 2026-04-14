7508dce6df7612b00ed9cca789813e1a6f400921
hardusss <danylo29bro@gmail.com>
2026-04-15 02:35:45 +0300

    set en lang

--- frontend/NextVibe/src/api/mapbox/get.coords.name.ts
+++ frontend/NextVibe/src/api/mapbox/get.coords.name.ts
@@ -1,15 +1,15 @@
 export const getLocationName = async (lng: number, lat: number) => {
     try {
-        const MAPBOX_TOKEN = process.env.EXPO_PUBLIC_MAPBOX_TOKEN; 
+        const MAPBOX_TOKEN = process.env.EXPO_PUBLIC_MAPBOX_TOKEN;
         const response = await fetch(
-            `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${MAPBOX_TOKEN}&types=poi,place,address&language=en`
+            `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${MAPBOX_TOKEN}&types=poi,place,address&language=en&worldview=US`
         );
         const data = await response.json();
-        
+
         if (data.features && data.features.length > 0) {
-            const placeName = data.features[0].place_name; 
-            
-            return (placeName.split(',')[0]); 
+            const placeName = data.features[0].place_name;
+
+            return (placeName.split(',')[0]);
         }
     } catch (error) {
         console.error("Geocoding error:", error);
