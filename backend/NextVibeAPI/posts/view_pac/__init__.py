fdcaef2b963030eab7e70b570c08d2821bd4a557
hardusss <danylo29bro@gmail.com>
2026-05-06 12:11:55 +0300

    feat: add import in init

--- backend/NextVibeAPI/posts/view_pac/__init__.py
+++ backend/NextVibeAPI/posts/view_pac/__init__.py
@@ -22,4 +22,4 @@ from .get_vibemap_events import GetVibemapEventsView
 from .luma_event import LumaEventPreviewView, LumaEventVerifyView
 from .event_requests import EventRequestCreateView, EventRequestListView, EventRequestActionView, EventAttendeesView
 from .event_checkin import EventCheckinView, EventCheckinListView, ClaimEventNftView
-from .event_connections import UserEventConnectionsView
+from .event_connections import UserEventConnectionsView, EventNFCConnectView
