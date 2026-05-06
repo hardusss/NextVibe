d5321a4616bd8f6bcc710476bae5dc9ceab806d8
hardusss <danylo29bro@gmail.com>
2026-05-07 02:44:35 +0300

    fix: add imports

--- backend/NextVibeAPI/user/views_pac/wallet_singin.py
+++ backend/NextVibeAPI/user/views_pac/wallet_singin.py
@@ -5,6 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from user.serializers_pac import UserWalletSignInSerializer
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class WalletSignInView(APIView):
     def post(self, request):
