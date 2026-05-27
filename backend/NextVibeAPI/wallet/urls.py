d2a41a2ea64553f650adb2ebf6c632d9d84fc745
hardusss <danylo29bro@gmail.com>
2026-05-27 20:43:04 +0300

    feat(wallet): add custom proxy to helius rpc

--- backend/NextVibeAPI/wallet/urls.py
+++ backend/NextVibeAPI/wallet/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .view import (
     GetTransactionFee,
-    GetTokensPriceView
+    GetTokensPriceView,
+    SolanaRpcProxyView,
     )
 
 
 urlpatterns = [
     path("fee/", GetTransactionFee.as_view(), name="fee"),
-    path("get-tokens-price/", GetTokensPriceView.as_view(), name="get_tokens_price")
+    path("get-tokens-price/", GetTokensPriceView.as_view(), name="get_tokens_price"),
+    path("rpc/", SolanaRpcProxyView.as_view(), name="solana_rpc_proxy"),
 ]
