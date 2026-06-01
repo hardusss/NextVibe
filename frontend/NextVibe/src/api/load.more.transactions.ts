6d81292e3cc3c03501083da2f3a10deaa074f46e
hardusss <danylo29bro@gmail.com>
2026-06-01 15:21:52 +0300

    feat: add button for fetch more transactions

--- frontend/NextVibe/src/api/load.more.transactions.ts
+++ frontend/NextVibe/src/api/load.more.transactions.ts
@@ -2,17 +2,16 @@ import axios from "axios";
 import { storage } from "../utils/storage";
 import GetApiUrl from "../utils/url_api";
 
-export default async function getTransactions (lastSignature?: string) {
+export default async function loadMoreTransactionsFromBlockchain(limit: number = 50) {
     const url = GetApiUrl();
     const token = await storage.getItem("access");
 
-    const params = lastSignature ? { lastSignature } : {};
-
-    const response = await axios.get(`${url}/wallets/transactions/`, {
+    const response = await axios.post(`${url}/wallets/transactions/load-more/`, 
+    { limit }, 
+    {
         headers: {
             Authorization: `Bearer ${token}`,
         },
-        params,
     });
 
     return response.data;
