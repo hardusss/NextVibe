abeb6c8ec03235b5ba6235982a14421e4002e1c0
hardusss <danylo29bro@gmail.com>
2026-07-08 00:48:26 +0300

    fix

--- frontend/NextVibe/withFmtConstevalFix.js
+++ frontend/NextVibe/withFmtConstevalFix.js
@@ -2,46 +2,33 @@ const { withDangerousMod } = require('@expo/config-plugins');
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function withFmtConstevalFix(config) {
+module.exports = function withFmtFix(config) {
   return withDangerousMod(config, [
     'ios',
     async (config) => {
-      const podfilePath = path.join(config.modRequest.projectRoot, 'ios/Podfile');
-      
-      if (!fs.existsSync(podfilePath)) {
-        console.log('[withFmtConstevalFix] Podfile does not exist, skipping');
-        return config;
-      }
-      
-      let podfileContent = fs.readFileSync(podfilePath, 'utf8');
-      
-      if (podfileContent.includes("target.name == 'fmt'")) {
-        console.log('[withFmtConstevalFix] fmt consteval fix already exists in Podfile');
-        return config;
-      }
-      
-      console.log('[withFmtConstevalFix] Injecting fmt consteval fix into Podfile...');
-      
-      const fixCode = `    installer.pods_project.targets.each do |target|
-      if target.name == 'fmt'
-        target.build_configurations.each do |config|
-          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
-          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FMT_CONSTEVAL='
-        end
+      const podfile = path.join(config.modRequest.platformProjectRoot, 'Podfile');
+      let contents = await fs.promises.readFile(podfile, 'utf8');
+
+      const fix = `
+  installer.pods_project.targets.each do |target|
+    if target.name == 'RCT-Folly' || target.name == 'fmt'
+      target.build_configurations.each do |config|
+        config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
+        config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FMT_CONSTEVAL='
       end
     end
+  end
+`;
 
-    react_native_post_install(`;
-      
-      podfileContent = podfileContent.replace(
-        /react_native_post_install\(/,
-        fixCode
-      );
-      
-      fs.writeFileSync(podfilePath, podfileContent, 'utf8');
-      console.log('[withFmtConstevalFix] Successfully patched Podfile');
-      
+      if (!contents.includes('FMT_CONSTEVAL=')) {
+        contents = contents.replace(
+          /post_install do \|installer\|/g,
+          `post_install do |installer|\n${fix}`
+        );
+        await fs.promises.writeFile(podfile, contents);
+      }
+
       return config;
-    }
+    },
   ]);
 };
