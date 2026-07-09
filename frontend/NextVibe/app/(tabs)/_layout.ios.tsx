f6b329742c13ce3a0e2e67f2d8bde52331a76d7e
hardusss <danylo29bro@gmail.com>
2026-07-09 20:01:36 +0300

    style(tabs): optimize native iOS tab bar triggers, icons, and label styling

--- frontend/NextVibe/app/(tabs)/_layout.ios.tsx
+++ frontend/NextVibe/app/(tabs)/_layout.ios.tsx
@@ -1,5 +1,4 @@
-// @ts-ignore
-import { NativeTabs, Icon } from "expo-router/unstable-native-tabs";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 import { ThemeProvider, DarkTheme, DefaultTheme } from "@react-navigation/native";
 import { useColorScheme, DynamicColorIOS } from "react-native";
 
@@ -9,26 +8,31 @@ export default function IOSTabsLayout() {
     return (
         <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
             <NativeTabs
-                tintColor={DynamicColorIOS({ dark: "#A855F7", light: "#7c3aed" })}
+                tintColor={DynamicColorIOS({ dark: "#ffffffff", light: "#000000ff" })}
             >
                 <NativeTabs.Trigger name="home">
-                    <Icon sf={{ default: "house", selected: "house.fill" }} />
+                    <NativeTabs.Trigger.Icon sf={{ default: "house", selected: "house.fill" }} />
+                    <NativeTabs.Trigger.Label>{""}</NativeTabs.Trigger.Label>
                 </NativeTabs.Trigger>
 
                 <NativeTabs.Trigger name="search">
-                    <Icon sf="magnifyingglass" />
+                    <NativeTabs.Trigger.Icon sf="magnifyingglass" />
+                    <NativeTabs.Trigger.Label>{""}</NativeTabs.Trigger.Label>
                 </NativeTabs.Trigger>
 
                 <NativeTabs.Trigger name="vibe-map">
-                    <Icon sf={{ default: "map", selected: "map.fill" }} />
+                    <NativeTabs.Trigger.Icon sf={{ default: "map", selected: "map.fill" }} />
+                    <NativeTabs.Trigger.Label>{""}</NativeTabs.Trigger.Label>
                 </NativeTabs.Trigger>
 
                 <NativeTabs.Trigger name="camera">
-                    <Icon sf={{ default: "plus.circle", selected: "plus.circle.fill" }} />
+                    <NativeTabs.Trigger.Icon sf={{ default: "plus.circle", selected: "plus.circle.fill" }} />
+                    <NativeTabs.Trigger.Label>{""}</NativeTabs.Trigger.Label>
                 </NativeTabs.Trigger>
 
                 <NativeTabs.Trigger name="profile">
-                    <Icon sf={{ default: "person.crop.circle", selected: "person.crop.circle.fill" }} />
+                    <NativeTabs.Trigger.Icon sf={{ default: "person.crop.circle", selected: "person.crop.circle.fill" }} />
+                    <NativeTabs.Trigger.Label>{""}</NativeTabs.Trigger.Label>
                 </NativeTabs.Trigger>
             </NativeTabs>
         </ThemeProvider>
