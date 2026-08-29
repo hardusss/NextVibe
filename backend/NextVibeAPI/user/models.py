bf6c72f90248dd6a1366250d990638456090ebc3
hardusss <danylo29bro@gmail.com>
2026-08-29 07:18:14 -0700

    fix

--- backend/NextVibeAPI/user/models.py
+++ backend/NextVibeAPI/user/models.py
@@ -57,7 +57,7 @@ class User(AbstractBaseUser):
     is_baned = models.BooleanField(default=False)
     last_activity = models.DateTimeField(default=timezone.now)
     wallet_address = models.CharField(unique=True, max_length=50, blank=True, null=True)
-    expo_push_token = models.CharField(max_length=512, blank=True, null=True)
+    expo_push_token = models.CharField(max_length=100, blank=True, null=True)
     muted_cherry_chat = models.BooleanField(default=False)
     from_invite_code = models.ForeignKey("InviteUser", on_delete=models.SET_NULL, null=True, blank=True, related_name="invited_users")
     auth_provider = models.CharField(max_length=20, null=True, blank=True, default="email")
